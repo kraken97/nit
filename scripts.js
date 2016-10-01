0ec9a1e27bfb50ebf56d87ca248f91b181f78abc
kraken <kraken@ex.ua>
2016-10-01 15:27:39 +0300

    rewrite logic with jquery
    Signed-off-by: kraken <kraken@ex.ua>

--- scripts.js
+++ scripts.js
@@ -1,95 +1,84 @@
-    var items = [{name:"test",count:1,active:true},{name:"test22",count:1,active:false},{name:"test2",count:1,active:true}]
+$(function () {
+    var defaultCount = 1;
+    var item = $(".item").hide().clone();
+    var stat = $(".stat").hide().clone();
+
+    $(".item-form").submit(function (e) {
+        e.preventDefault();
+        var fieldValue = $(this).serializeArray()[0].value;
+        $(this)[0].reset();
+        addItem(fieldValue);
+
+        
+    })
+
+    function addItem(fieldValue){
+        if (fieldValue) {
+
+            var elem = item.clone();
+            var stat_elem = stat.clone();
+            var counter = $(elem).find(".counter")
+            var stat_num = stat_elem.find(".num");
+            var stat_text = stat_elem.find(".title");
+            var decr_butt=  $(elem).find(".decr")
+            var itemname = $(elem).find(".itemname")
+
+            elem.find(".del").click(function (e) {
+                elem.fadeOut(400,function(){
+                    elem.remove();
+                     stat_elem.remove();
+                })
+               
+            });
+
+        
+            itemname.text(fieldValue);
+            itemname.click(function () {
+                $(this).attr("contenteditable", "true");
+            }).focusout(function () {
+                var itemtext = itemname.text();
+                itemname.text(itemname.text());
+                stat_text.text(itemtext);
+            });
+
+            stat_text.text(fieldValue);
+            counter.text(defaultCount);
+            stat_num.text(defaultCount);
+
+            $(elem).find(".inc").click(function (e) {
+                var num = parseInt(counter.text()) + 1;
+                counter.text(num);
+                stat_num.text(num);
+                decr_butt.prop("disabled", num <= 1);
 
 
+            })
+            decr_butt.click(function (e) {
+                var counter_num = parseInt(counter.text());
+                var num = counter_num > 1 ? counter_num - 1 : 1;
+                decr_butt.prop("disabled", num <= 1);
+                counter.text(num);
+                stat_num.text(num);
+            });
 
+            elem.find(".sold").click(function (e) {
+                $(".content-sold").append(stat_elem);
+                elem.toggleClass("solded");
+            });
+             elem.find(".unsold").click(function (e) {
+                $(".content-availble").append(stat_elem);
+                elem.toggleClass("solded");
+            });
 
-function inc(i){
-items[i].count=items[i].count+1;
-update();
-}
-function decr(i){
-   
-    items[i].count=items[i].count>1?items[i].count-1:1;
-     update();
-}
+            console.log(elem);
+            $(".item-container").append(elem.fadeIn());
+            $(".content-availble").append(stat_elem.show());
 
-function remove(i){
-    items.splice(i,1);
-    update();
-
-}
-function toggleSold(i){
-    items[i].active=!items[i].active;
-    update();
-}
-function add(e){
-    e.preventDefault(); 
-    var name=e.target.elements["name"].value
-    e.target.elements["name"].value='';
-    if(name!=''){
-            items.push({name:name,count:1,active:true});
-             update();
-    }
-}
- 
-function update(){
-    var container=document.querySelector('.item-container');
-     var containerAv=document.querySelector('.content-availble');
-        var containerS=document.querySelector('.content-sold');
-    container.innerHTML='';
-    containerAv.innerHTML='';
-    containerS.innerHTML='';
-
-    for(var i=0;i<items.length;i++){
-        var item={name:items[i].name,count:items[i].count,active:items[i].active};
-    container.appendChild(createItem( item,i));
-    if(item.active){
-         containerAv.appendChild(createBadge(item,i));
-
-    }else{
-        containerS.appendChild(createBadge(item,1));
         }
     }
-}
 
+    addItem("pim")
+    addItem("pom")
+    addItem("pum")
 
-function createBadge(item,index){
-    var newcontent=document.createElement('span');
-    newcontent.className="p-2 gray small-border m-4 relative";
-
-    newcontent.innerHTML= `
-                       <span class="title">${item.name}</span> 
-                       <span class="small-border orange p-3 floating-label">${item.count}
-                       `
-                       return newcontent;
-}
-
-function createItem(item,index){
-     var newcontent = document.createElement('div');
- 
-     var hide=item.active?'':'hidden';
-     var isSolded=item.active?'Buy':'Sold';
-     var buyColor=item.active?'blue':'gray-dark';
-     
-    newcontent.innerHTML = `
-        <div class="row flex space-b p-1 wrap baseline availble item">
-                <div class="itemname">${item.name}</div>
-                <div class="center  ">
-                    <button class="green inc tooltip ${hide}" onclick="inc(${index})" style="visibility">+</button>
-                    <span class="small-border gray p-2 counter">${item.count}</span> 
-                    <button class="decr red tooltip ${hide}" onclick="decr(${index})" style="visibility" ${item.count<=1?'disabled':''}>-</button>
-                </div>
-                <div class="left">
-                    <button class="ui-button ${buyColor}" onclick="toggleSold(${index})">${isSolded}</button>
-                    <button class="ui-button red del ${hide}"  onclick="remove(${index})"  >X</button>
-                </div>
-            </div>
-            `
-            return newcontent;
-}
-
-
-document.addEventListener('DOMContentLoaded',function(){
-
-    update();
-})
+});
